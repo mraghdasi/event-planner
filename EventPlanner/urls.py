7d8868946eeee9c530644c346856d8bf5ed5c84b
soroush moradpour <46293975+soroushmp@users.noreply.github.com>
2024-03-03 03:25:47 +0330

    1. Add Static File of Templates 2. Debug Template 3. Add Homepage to views.py 4. Modify urls.py 5. Add base.html 6. Add example.html "Note: base.html Is Not Clean!"

--- EventPlanner/urls.py
+++ EventPlanner/urls.py
@@ -1,6 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from EventPlanner import settings
+from main import views as ma
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('', ma.homepage, name='homepage')
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
