8ad484a374675611749bb9e0328f75e8ca4d59d3
Foroutan Aghdasi <mr.aghdasi33@gmail.com>
2024-03-07 02:55:35 +0330

    room list -- room detail

--- meetings/urls.py
+++ meetings/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from EventPlanner import settings
 from meetings import views
 
+app_name = 'meeting'
+
 urlpatterns = [
-                  path('', views.homepage, name='homepage'),
-                  path('more/', views.more, name='more'),
+                  path('room/<id>/', views.RoomDetail.as_view(), name='room_detail'),
+                  path('', views.RoomList.as_view(), name='room_list'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- meetings/views.py
+++ meetings/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+from django.db.models import Count
+
+from meetings.models import Room
 
 
-def more(request):
-    return render(request, 'meeting/example.html', {})
+class RoomList(View):
+    def get(self, request):
+        rooms = Room.objects.annotate(meeting_count=Count('meetings'))
+        return render(request, 'meeting/room_list.html', {'rooms': rooms})
 
 
-def homepage(request):
-    return render(request, 'meeting/example.html', {})
+class RoomDetail(View):
+    def get(self, request, id):
+        room = Room.objects.filter(id=id).annotate(meeting_count=Count('meetings'))
+
+        if not room.exists():
+            room = None
+        else:
+            room = room[0]
+        return render(request, 'meeting/room_detail.html', {'room': room})
