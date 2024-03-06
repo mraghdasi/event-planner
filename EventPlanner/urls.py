571fd25653c44965d70e39e66a21e650640772be
mohamadrexa <yazdanifar@hotmail.com>
2024-03-06 10:17:17 -1000

    Admin Dashboard

--- EventPlanner/urls.py
+++ EventPlanner/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('users/', include('users.urls')),
                   path('meetings/', include('meetings.urls')),
+                  path('admin_dash/', include('admin_dash.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
